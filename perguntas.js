f088c9bb64835b179fd650e552300f52be008716
Brayan-Guilherme <161321907+Brayan-Guilherme@users.noreply.github.com>
2024-11-04 08:15:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,56 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'curiosidades',
+    'qual o mánga com maior números de vendas do mundo?',
+    ' one piece'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz  em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'animais',
+    'qual o animal mais venenoso do mundo ?',
+    'é a água viva vespa-do-mar (mesmo medindo apenas 30mm no maximo seus tentaculos podem medir ate 3 metros de comprimento).'
+)
+
+
+criaCartao(
+    'astronomia',
+    'qual a maio estrela do universo?',
+    'Vy canis majoris, sendo 2.100 vezes maior que o sol em diãmetro.'
+)
+
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a área de um triângulo?',
+    'A área de um triângulo qualquer pode ser calculada, multiplicando a base pela sua altura e dividindo esse resultado por 2.'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a área de um triângulo?',
+    'A área de um triângulo qualquer pode ser calculada, multiplicando a base pela sua altura e dividindo esse resultado por 2.'
+)
+
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a área de um triângulo?',
+    'A área de um triângulo qualquer pode ser calculada, multiplicando a base pela sua altura e dividindo esse resultado por 2.'
+)
